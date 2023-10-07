b4a9acb3f87efaeb7316a73e510f5aa50836859b
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-10-07 14:59:38 +0300

    build singly linked list

--- app.js
+++ app.js
@@ -1,22 +1,87 @@
-// function bad(arr: number[]) {
-//   for (const i in arr) {
-//     for (const j in arr) {
-//       if (arr[i] === arr[j] && i !== j) {
-//         return arr[i];
-//       }
-//     }
-//   }
-// }
-function firstRecurringCharacter(arr) {
-    var haveSeen = {};
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var num = arr_1[_i];
-        if (haveSeen[num])
-            return num;
-        haveSeen[num] = 1;
+var ListNode = /** @class */ (function () {
+    function ListNode(value, next) {
+        if (next === void 0) { next = null; }
+        this.value = value;
+        this.next = next;
     }
-}
-console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 3, 4, 5]));
-console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+    return ListNode;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList(value) {
+        this.head = new ListNode(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+    LinkedList.prototype.append = function (value) {
+        var newNode = new ListNode(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    };
+    LinkedList.prototype.prepend = function (value) {
+        var newNode = new ListNode(value, this.head);
+        this.head = newNode;
+        this.length++;
+    };
+    LinkedList.prototype.printList = function () {
+        var list = [];
+        var currentNode = this.head;
+        while (currentNode !== null) {
+            list.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(list);
+    };
+    LinkedList.prototype.goToIndex = function (index) {
+        if (index === 0)
+            return this.head;
+        if (index === this.length - 1)
+            return this.tail;
+        var currentNode = this.head;
+        for (var i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    };
+    LinkedList.prototype.insert = function (index, value) {
+        if (index === 0)
+            return this.prepend(value);
+        if (index === this.length)
+            return this.append(value);
+        if (index < 0 || index > this.length)
+            return console.log("invalid index");
+        var prevNode = this.goToIndex(index - 1);
+        var nextNode = prevNode.next;
+        var newNode = new ListNode(value, nextNode);
+        prevNode.next = newNode;
+        this.length++;
+    };
+    LinkedList.prototype.remove = function (index) {
+        if (index === 0) {
+            if (this.length === 1)
+                return console.log("there is only one element");
+            var newHead = this.head.next;
+            this.head = newHead;
+            this.length--;
+            return;
+        }
+        if (index < 0 || index >= this.length)
+            return console.log("invalid index");
+        var prevNode = this.goToIndex(index - 1);
+        var nextNode = prevNode.next.next;
+        prevNode.next = nextNode;
+        if (nextNode === null)
+            this.tail = prevNode;
+        this.length--;
+    };
+    return LinkedList;
+}());
+var myLinkedList = new LinkedList(10);
+myLinkedList.append(20);
+myLinkedList.prepend(50);
+myLinkedList.append(60);
+myLinkedList.insert(3, 30);
+myLinkedList.printList();
+myLinkedList.remove(4);
+myLinkedList.printList();
+console.log(myLinkedList.tail);
--- app.ts
+++ app.ts
@@ -1,22 +1,88 @@
-// function bad(arr: number[]) {
-//   for (const i in arr) {
-//     for (const j in arr) {
-//       if (arr[i] === arr[j] && i !== j) {
-//         return arr[i];
-//       }
-//     }
-//   }
-// }
+class ListNode {
+  constructor(public value: any, public next: ListNode | null = null) {}
+}
+class LinkedList {
+  head: ListNode;
+  tail: ListNode;
+  length: number;
+  constructor(value: any) {
+    this.head = new ListNode(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value: any) {
+    let newNode = new ListNode(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value: any) {
+    let newNode = new ListNode(value, this.head);
+    this.head = newNode;
+    this.length++;
+  }
 
-function firstRecurringCharacter(arr: number[]) {
-  let haveSeen = {};
-  for (const num of arr) {
-    if (haveSeen[num]) return num;
-    haveSeen[num] = 1;
+  printList() {
+    let list: any[] = [];
+    let currentNode: ListNode | null = this.head;
+    while (currentNode !== null) {
+      list.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(list);
+  }
+
+  goToIndex(index: number) {
+    if (index === 0) return this.head;
+    if (index === this.length - 1) return this.tail;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next!;
+    }
+    return currentNode;
+  }
+
+  insert(index: number, value: any) {
+    if (index === 0) return this.prepend(value);
+    if (index === this.length) return this.append(value);
+    if (index < 0 || index > this.length) return console.log("invalid index");
+
+    let prevNode = this.goToIndex(index - 1);
+    let nextNode = prevNode.next;
+    let newNode = new ListNode(value, nextNode);
+    prevNode.next = newNode;
+
+    this.length++;
+  }
+
+  remove(index: number) {
+    if (index === 0) {
+      if (this.length === 1) return console.log("there is only one element");
+      let newHead = this.head.next;
+      this.head = newHead!;
+      this.length--;
+      return;
+    }
+
+    if (index < 0 || index >= this.length) return console.log("invalid index");
+
+    let prevNode = this.goToIndex(index - 1);
+    let nextNode = prevNode.next!.next;
+
+    prevNode.next = nextNode;
+    if (nextNode === null) this.tail = prevNode;
+    this.length--;
   }
 }
 
-console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 3, 4, 5]));
-console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(20);
+myLinkedList.prepend(50);
+myLinkedList.append(60);
+
+myLinkedList.insert(3, 30);
+myLinkedList.printList();
+myLinkedList.remove(4);
+myLinkedList.printList();
+
+console.log(myLinkedList.tail);
