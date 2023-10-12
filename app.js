f959434f145d63a1bf42f11d4418233009a02cb1
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-10-12 15:33:14 +0300

    linked list and doubly linked list in the same file

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var ListNode = /** @class */ (function () {
-    function ListNode(value, next, prev) {
+    function ListNode(value, next) {
+        if (next === void 0) { next = null; }
+        this.value = value;
+        this.next = next;
+    }
+    return ListNode;
+}());
+var DoublyListNode = /** @class */ (function () {
+    function DoublyListNode(value, next, prev) {
         if (next === void 0) { next = null; }
         if (prev === void 0) { prev = null; }
         this.value = value;
         this.next = next;
         this.prev = prev;
     }
-    return ListNode;
+    return DoublyListNode;
 }());
 var LinkedList = /** @class */ (function () {
     function LinkedList(value) {
@@ -16,14 +24,12 @@ var LinkedList = /** @class */ (function () {
     }
     LinkedList.prototype.append = function (value) {
         var newNode = new ListNode(value);
-        newNode.prev = this.tail;
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
     };
     LinkedList.prototype.prepend = function (value) {
         var newNode = new ListNode(value, this.head);
-        this.head.prev = newNode;
         this.head = newNode;
         this.length++;
     };
@@ -37,6 +43,78 @@ var LinkedList = /** @class */ (function () {
         console.log(list);
     };
     LinkedList.prototype.goToIndex = function (index) {
+        if (index === 0)
+            return this.head;
+        if (index === this.length - 1)
+            return this.tail;
+        var currentNode = this.head;
+        for (var i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    };
+    LinkedList.prototype.insert = function (index, value) {
+        if (index === 0)
+            return this.prepend(value);
+        if (index === this.length)
+            return this.append(value);
+        if (index < 0 || index > this.length)
+            return console.log("invalid index");
+        var prevNode = this.goToIndex(index - 1);
+        var nextNode = prevNode.next;
+        var newNode = new ListNode(value, nextNode);
+        prevNode.next = newNode;
+        this.length++;
+    };
+    LinkedList.prototype.remove = function (index) {
+        if (index === 0) {
+            if (this.length === 1)
+                return console.log("there is only one element");
+            var newHead = this.head.next;
+            this.head = newHead;
+            this.length--;
+            return;
+        }
+        if (index < 0 || index >= this.length)
+            return console.log("invalid index");
+        var prevNode = this.goToIndex(index - 1);
+        var nextNode = prevNode.next.next;
+        prevNode.next = nextNode;
+        if (nextNode === null)
+            this.tail = prevNode;
+        this.length--;
+    };
+    return LinkedList;
+}());
+var DoublyLinkedList = /** @class */ (function () {
+    function DoublyLinkedList(value) {
+        this.head = new DoublyListNode(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+    DoublyLinkedList.prototype.append = function (value) {
+        var newNode = new DoublyListNode(value);
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    };
+    DoublyLinkedList.prototype.prepend = function (value) {
+        var newNode = new DoublyListNode(value, this.head);
+        this.head.prev = newNode;
+        this.head = newNode;
+        this.length++;
+    };
+    DoublyLinkedList.prototype.printList = function () {
+        var list = [];
+        var currentNode = this.head;
+        while (currentNode !== null) {
+            list.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(list);
+    };
+    DoublyLinkedList.prototype.goToIndex = function (index) {
         if (index === 0)
             return this.head;
         if (index === this.length - 1)
@@ -56,7 +134,7 @@ var LinkedList = /** @class */ (function () {
         }
         return currentNode;
     };
-    LinkedList.prototype.insert = function (index, value) {
+    DoublyLinkedList.prototype.insert = function (index, value) {
         if (index === 0)
             return this.prepend(value);
         if (index === this.length)
@@ -65,12 +143,12 @@ var LinkedList = /** @class */ (function () {
             return console.log("invalid index");
         var prevNode = this.goToIndex(index - 1);
         var nextNode = prevNode.next;
-        var newNode = new ListNode(value, nextNode, prevNode);
+        var newNode = new DoublyListNode(value, nextNode, prevNode);
         prevNode.next = newNode;
         nextNode.prev = newNode;
         this.length++;
     };
-    LinkedList.prototype.remove = function (index) {
+    DoublyLinkedList.prototype.remove = function (index) {
         if (this.length === 1)
             return console.log("there is only one element");
         if (index === 0) {
@@ -95,9 +173,9 @@ var LinkedList = /** @class */ (function () {
         nextNode.prev = prevNode;
         this.length--;
     };
-    return LinkedList;
+    return DoublyLinkedList;
 }());
-var myLinkedList = new LinkedList(0);
+var myLinkedList = new DoublyLinkedList(0);
 myLinkedList.append(10);
 myLinkedList.prepend(-10);
 myLinkedList.append(20);
--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 class ListNode {
+  constructor(public value: any, public next: ListNode | null = null) {}
+}
+
+class DoublyListNode {
   constructor(
     public value: any,
-    public next: ListNode | null = null,
-    public prev: ListNode | null = null
+    public next: DoublyListNode | null = null,
+    public prev: DoublyListNode | null = null
   ) {}
 }
+
 class LinkedList {
   head: ListNode;
   tail: ListNode;
@@ -16,14 +21,12 @@ class LinkedList {
   }
   append(value: any) {
     let newNode = new ListNode(value);
-    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   }
   prepend(value: any) {
     let newNode = new ListNode(value, this.head);
-    this.head.prev = newNode;
     this.head = newNode;
     this.length++;
   }
@@ -41,8 +44,84 @@ class LinkedList {
   goToIndex(index: number) {
     if (index === 0) return this.head;
     if (index === this.length - 1) return this.tail;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next!;
+    }
+    return currentNode;
+  }
 
-    let currentNode: ListNode;
+  insert(index: number, value: any) {
+    if (index === 0) return this.prepend(value);
+    if (index === this.length) return this.append(value);
+    if (index < 0 || index > this.length) return console.log("invalid index");
+
+    let prevNode = this.goToIndex(index - 1);
+    let nextNode = prevNode.next;
+    let newNode = new ListNode(value, nextNode);
+    prevNode.next = newNode;
+
+    this.length++;
+  }
+
+  remove(index: number) {
+    if (index === 0) {
+      if (this.length === 1) return console.log("there is only one element");
+      let newHead = this.head.next;
+      this.head = newHead!;
+      this.length--;
+      return;
+    }
+
+    if (index < 0 || index >= this.length) return console.log("invalid index");
+
+    let prevNode = this.goToIndex(index - 1);
+    let nextNode = prevNode.next!.next;
+
+    prevNode.next = nextNode;
+    if (nextNode === null) this.tail = prevNode;
+    this.length--;
+  }
+}
+
+class DoublyLinkedList {
+  head: DoublyListNode;
+  tail: DoublyListNode;
+  length: number;
+  constructor(value: any) {
+    this.head = new DoublyListNode(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value: any) {
+    let newNode = new DoublyListNode(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value: any) {
+    let newNode = new DoublyListNode(value, this.head);
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+  }
+
+  printList() {
+    let list: any[] = [];
+    let currentNode: DoublyListNode | null = this.head;
+    while (currentNode !== null) {
+      list.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(list);
+  }
+
+  goToIndex(index: number) {
+    if (index === 0) return this.head;
+    if (index === this.length - 1) return this.tail;
+
+    let currentNode: DoublyListNode;
     if (index < this.length / 2) {
       currentNode = this.head;
 
@@ -66,7 +145,7 @@ class LinkedList {
 
     let prevNode = this.goToIndex(index - 1);
     let nextNode = prevNode.next;
-    let newNode = new ListNode(value, nextNode, prevNode);
+    let newNode = new DoublyListNode(value, nextNode, prevNode);
     prevNode.next = newNode;
     nextNode!.prev = newNode;
 
@@ -102,7 +181,7 @@ class LinkedList {
   }
 }
 
-let myLinkedList = new LinkedList(0);
+let myLinkedList = new DoublyLinkedList(0);
 myLinkedList.append(10);
 myLinkedList.prepend(-10);
 myLinkedList.append(20);
