7632b547c3ec9e1baf5bae1b0a26aa56a119b8a6
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-10-15 14:07:23 +0300

    binary search tree (insert - search - remove)

--- app.js
+++ app.js
@@ -1,51 +1,115 @@
-var ListNode = /** @class */ (function () {
-    function ListNode(value, next) {
-        if (next === void 0) { next = null; }
+var TreeNode = /** @class */ (function () {
+    function TreeNode(value, left, right, frequency) {
+        if (left === void 0) { left = null; }
+        if (right === void 0) { right = null; }
+        if (frequency === void 0) { frequency = 1; }
         this.value = value;
-        this.next = next;
+        this.left = left;
+        this.right = right;
+        this.frequency = frequency;
     }
-    return ListNode;
+    return TreeNode;
 }());
-var Queue = /** @class */ (function () {
-    function Queue() {
-        this.first = null;
-        this.last = null;
-        this.length = 0;
+var BinarySearchTree = /** @class */ (function () {
+    function BinarySearchTree() {
+        this.root = null;
     }
-    Queue.prototype.peek = function () {
-        return this.first;
-    };
-    Queue.prototype.enqueue = function (value) {
-        var newNode = new ListNode(value);
-        if (!this.last) {
-            this.first = newNode;
-            this.last = newNode;
+    BinarySearchTree.prototype.findTargetNode = function (value) {
+        if (!this.root)
+            return { currentNode: null, prevNode: null };
+        var currentNode = this.root;
+        var prevNode = null;
+        while ((currentNode.left && value < currentNode.value) ||
+            (currentNode.right && value > currentNode.value)) {
+            prevNode = currentNode;
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            }
+            else {
+                currentNode = currentNode.right;
+            }
         }
-        else {
-            this.last.next = newNode;
-            this.last = newNode;
+        return { currentNode: currentNode, prevNode: prevNode };
+    };
+    BinarySearchTree.prototype.insert = function (value) {
+        var newNode = new TreeNode(value);
+        var currentNode = this.findTargetNode(value).currentNode;
+        if (!currentNode)
+            return (this.root = newNode);
+        if (value < currentNode.value)
+            currentNode.left = newNode;
+        if (value > currentNode.value)
+            currentNode.right = newNode;
+        if (value === currentNode.value)
+            currentNode.frequency++;
+    };
+    BinarySearchTree.prototype.lookup = function (value) {
+        var currentNode = this.findTargetNode(value).currentNode;
+        if (!currentNode)
+            return null;
+        return value === currentNode.value ? currentNode : null;
+    };
+    BinarySearchTree.prototype.remove = function (value) {
+        var _a = this.findTargetNode(value), currentNode = _a.currentNode, prevNode = _a.prevNode;
+        if (!currentNode)
+            return false;
+        // if frequency
+        if (currentNode.frequency > 1)
+            return currentNode.frequency--;
+        // if leaf
+        if (!currentNode.left && !currentNode.right) {
+            if (currentNode === this.root || prevNode === null)
+                return (this.root = null);
+            if (value < prevNode.value)
+                return (prevNode.left = null);
+            if (value > prevNode.value)
+                return (prevNode.right = null);
         }
-        this.length++;
+        // if has one child
+        if (!currentNode.right || !currentNode.left) {
+            if (currentNode === this.root || prevNode === null)
+                return (this.root = this.root.left || this.root.right);
+            if (value < prevNode.value)
+                return (prevNode.left = currentNode.left || currentNode.right);
+            if (value > prevNode.value)
+                return (prevNode.right = currentNode.left || currentNode.right);
+        }
+        // now it definitely has two childs
+        var successor = currentNode.right;
+        while (successor.left) {
+            successor = successor.left;
+        }
+        this.remove(successor.value);
+        currentNode.value = successor.value;
     };
-    Queue.prototype.dequeue = function () {
-        if (!this.first)
-            return;
-        if (this.length === 1)
-            this.last = null;
-        this.first = this.first.next;
-        this.length--;
-    };
-    return Queue;
+    return BinarySearchTree;
 }());
-var myQueue = new Queue();
-myQueue.enqueue(1);
-myQueue.enqueue(2);
-myQueue.enqueue(3);
-myQueue.dequeue();
-myQueue.dequeue();
-// myQueue.dequeue();
-console.log(myQueue);
-//Joy
-//Matt
-//Pavel
-//Samir
+var tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+// tree.insert(1);
+tree.remove(1);
+// tree.remove(6);
+// tree.remove(15);
+// tree.remove(170);
+// tree.remove(4);
+// tree.remove(20);
+// tree.remove(9);
+console.log(tree.root);
+//     9
+//  4     20
+//1  6  15  170
+// console.log(traverse(tree.root));
+// function traverse(node: any) {
+//   const tree = { value: node.value };
+//   //@ts-ignore
+//   tree.left = node.left === null ? null : traverse(node.left);
+//   //@ts-ignore
+//   tree.right = node.right === null ? null : traverse(node.right);
+//   return tree;
+// }
--- app.ts
+++ app.ts
@@ -1,51 +1,119 @@
-class ListNode {
-  constructor(public value: any, public next: ListNode | null = null) {}
+class TreeNode {
+  constructor(
+    public value: any,
+    public left: TreeNode | null = null,
+    public right: TreeNode | null = null ,
+    public frequency: number = 1 ,
+  ) {}
 }
 
-class Queue {
-  first: ListNode | null;
-  last: ListNode | null;
-  length: number;
+class BinarySearchTree {
+  root: TreeNode | null;
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
+    this.root = null;
   }
-  peek() {
-    return this.first;
-  }
-  enqueue(value: any) {
-    let newNode = new ListNode(value);
-    if (!this.last) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
+
+  findTargetNode(value: any) {
+    if (!this.root) return { currentNode: null, prevNode: null };
+    let currentNode: TreeNode = this.root;
+    let prevNode: TreeNode | null = null;
+    while (
+      (currentNode.left && value < currentNode.value) ||
+      (currentNode.right && value > currentNode.value)
+    ) {
+      prevNode = currentNode;
+      if (value < currentNode.value) {
+        currentNode = currentNode.left!;
+      } else {
+        currentNode = currentNode.right!;
+      }
     }
-    this.length++;
+    return { currentNode, prevNode };
   }
-  dequeue() {
-    if (!this.first) return;
-    if (this.length === 1) this.last = null;
-    this.first = this.first.next;
-    this.length--;
+
+  insert(value: any) {
+    let newNode = new TreeNode(value);
+    let { currentNode } = this.findTargetNode(value);
+    if (!currentNode) return (this.root = newNode);
+
+    if (value < currentNode.value) currentNode.left = newNode;
+    if (value > currentNode.value) currentNode.right = newNode;
+    if (value === currentNode.value) currentNode.frequency ++;
+  }
+
+  lookup(value: any) {
+    let { currentNode } = this.findTargetNode(value);
+    if (!currentNode) return null;
+
+    return value === currentNode.value ? currentNode : null;
+  }
+  remove(value: any) {
+    let { currentNode, prevNode } = this.findTargetNode(value);
+    if (!currentNode) return false;
+
+    // if frequency
+    if(currentNode.frequency > 1) return currentNode.frequency --
+
+    // if leaf
+    if (!currentNode.left && !currentNode.right) {
+      if (currentNode === this.root || prevNode === null)
+        return (this.root = null);
+
+      if (value < prevNode.value) return (prevNode.left = null);
+      if (value > prevNode.value) return (prevNode.right = null);
+    }
+
+    // if has one child
+    if (!currentNode.right || !currentNode.left) {
+      if (currentNode === this.root || prevNode === null)
+        return (this.root = this.root!.left || this.root!.right);
+
+      if (value < prevNode.value)
+        return (prevNode.left = currentNode.left || currentNode.right);
+      if (value > prevNode.value)
+        return (prevNode.right = currentNode.left || currentNode.right);
+    }
+
+    // now it definitely has two childs
+    let successor = currentNode.right!;
+    while (successor.left) {
+      successor = successor.left;
+    }
+    this.remove(successor.value);
+    currentNode.value = successor.value;
   }
 }
 
-const myQueue = new Queue();
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+// tree.insert(1);
 
-myQueue.enqueue(1);
-myQueue.enqueue(2);
-myQueue.enqueue(3);
+tree.remove(1);
+// tree.remove(6);
+// tree.remove(15);
+// tree.remove(170);
+// tree.remove(4);
+// tree.remove(20);
+// tree.remove(9);
+console.log(tree.root);
 
-myQueue.dequeue();
-myQueue.dequeue();
-// myQueue.dequeue();
+//     9
+//  4     20
+//1  6  15  170
 
-console.log(myQueue);
+// console.log(traverse(tree.root));
 
-//Joy
-//Matt
-//Pavel
-//Samir
+// function traverse(node: any) {
+//   const tree = { value: node.value };
+//   //@ts-ignore
+//   tree.left = node.left === null ? null : traverse(node.left);
+//   //@ts-ignore
+//   tree.right = node.right === null ? null : traverse(node.right);
+//   return tree;
+// }
